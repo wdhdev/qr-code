3b719b5c07ed92d4e16e99781b3657433914a120
William Harrison <william@williamdavidharrison.com.au>
2023-02-27 13:57:57 +0800

    Update generate.js

--- js/generate.js
+++ js/generate.js
@@ -1,8 +1,7 @@
 const wrapper = document.querySelector(".wrapper"),
-
-qrInput = wrapper.querySelector(".form input"),
-generateBtn = wrapper.querySelector(".form button"),
-qrImg = wrapper.querySelector(".qr-code img");
+    qrInput = wrapper.querySelector(".form input"),
+    generateBtn = wrapper.querySelector(".form button"),
+    qrImg = wrapper.querySelector(".qr-code img");
 
 let preValue;
 
